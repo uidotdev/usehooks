a3fdb86bd3bd19990e1105fc427fce033175d4d4
Gabe Ragland <gabe.ragland@gmail.com>
2019-03-21 11:58:18 -0700

    Fix post ordering

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.createPages = ({ actions, graphql }) => {
   return graphql(`
     {
       allMarkdownRemark(
-        sort: { order: ASC, fields: [frontmatter___date] }
+        sort: { order: DESC, fields: [frontmatter___date] }
         limit: 1000
       ) {
         edges {
