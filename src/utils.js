287fe1beb8190d47b3a7699263aae8cbda1c0bb2
Tucker <trblackw@ncsu.edu>
2019-05-05 11:58:16 -0400

    add email validator func

---
+++ src/utils.js
@@ -0,0 +1,4 @@
+export const validateEmail = email => {
+   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return regex.test(String(email).toLowerCase());
+};
