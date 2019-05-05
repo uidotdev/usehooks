4b31d59fbfb44a12f37a29f2b1fc2410fbcf4493
Tucker <trblackw@ncsu.edu>
2019-05-05 11:59:42 -0400

    reformat onchange prop usage

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,19 @@
-import React from "react";
+import React from 'react';
 
-export default ({ value, onChange }) => {
-  return (
-    <div style={{ maxWidth: "300px", margin: "40px auto 0 auto" }}>
-      <div className="control has-icons-left">
-        <input
-          style={{ borderColor: "#078b61" }}
-          className="input is-medium"
-          type="text"
-          placeholder="Search"
-          value={value}
-          onChange={({ target: { value } }) => onChange(value)}
-        />
-        <span className="icon is-small is-left">
-          <i className="fas fa-search" />
-        </span>
+export default ({ value, onChange }) => (
+   <div style={{ maxWidth: '300px', margin: '40px auto 0 auto' }}>
+      <div className='control has-icons-left'>
+         <input
+            style={{ borderColor: '#078b61' }}
+            className='input is-medium'
+            type='text'
+            placeholder='Search'
+            value={value}
+            onChange={e => onChange(e.target.value)}
+         />
+         <span className='icon is-small is-left'>
+            <i className='fas fa-search' />
+         </span>
       </div>
-    </div>
-  );
-};
+   </div>
+);
