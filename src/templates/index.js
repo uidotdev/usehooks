3e0b7f70b350c9e1d3ea61ae99e2fe40ead6fb13
Tucker <trblackw@ncsu.edu>
2019-05-06 09:15:10 -0400

    revert pagination links and add basic search functionality

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import Search from '../components/Search';
@@ -6,7 +6,7 @@ import { PostTemplate } from '../templates/post.js';
 
 const PaginationPrevious = (first, previousUrl) => {
    return first ? (
-      <a href='#' style={{ textDecoration: 'none', color: 'inherit' }}>
+      <a href='#' className='pagination-previous'>
          Previous
       </a>
    ) : (
@@ -18,28 +18,51 @@ const PaginationPrevious = (first, previousUrl) => {
 
 const PaginationNext = (last, nextUrl) => {
    return last ? (
-      <a href='#' style={{ textDecoration: 'none', color: 'inherit' }}>
+      <a href='#' className='pagination-next'>
          Next Page
       </a>
    ) : (
-      <Link className='pagination-previous' to={nextUrl}>
-         Previous
+      <Link className='pagination-next' to={nextUrl}>
+         Next Page
       </Link>
    );
 };
 
 const IndexPage = ({ pageContext }) => {
    const [search, setSearch] = useState('');
+   const [searchMatches, setSearchMatches] = useState([]);
    const { group, index, first, last, pageCount } = pageContext;
    const previousUrl = index - 1 == 1 ? '' : '/page/' + (index - 1).toString();
    const nextUrl = '/page/' + (index + 1).toString();
+
+   const searchPosts = () => {
+      const query = search !== '' && new RegExp(search, 'gi');
+      //considering we're searching for hooks, 'use' probably isn't going to be very helpful
+      if (!query || query === 'use') return;
+      const matches = group.filter(({ node: { frontmatter: { title } } }) => query.test(title));
+      setSearchMatches(matches);
+   };
+
+   useEffect(
+      () => {
+         searchPosts();
+      },
+      [search]
+   );
+
    return (
       <Layout>
-         {/* <Search value={search} onChange={setSearch} /> */}
+         <Search value={search} onChange={setSearch} />
 
-         {group.map(({ node }) => (
-            <PostTemplate key={node.id} content={node.html} frontmatter={node.frontmatter} slug={node.fields.slug} />
-         ))}
+         {search === '' ? (
+            group.map(({ node }) => (
+               <PostTemplate key={node.id} content={node.html} frontmatter={node.frontmatter} slug={node.fields.slug} />
+            ))
+         ) : (
+            searchMatches.map(({ node }) => (
+               <PostTemplate key={node.id} content={node.html} frontmatter={node.frontmatter} slug={node.fields.slug} />
+            ))
+         )}
 
          <nav
             className='pagination is-centered'
@@ -49,8 +72,31 @@ const IndexPage = ({ pageContext }) => {
                maxWidth: '600px',
                margin: '0 auto'
             }}>
-            <PaginationPrevious first={first} previousUrl={previousUrl} />
-            <PaginationNext last={last} nextUrl={nextUrl} />
+            <Link
+               className='pagination-previous'
+               to={previousUrl}
+               disabled={first}
+               style={{
+                  // Temp hack to prevent clicking
+                  // TODO: Render a link without a href instead
+                  'pointer-events': first ? 'none' : 'auto'
+               }}>
+               Previous
+            </Link>
+            {/* //if there's a search taking place and less than 10 results, don't present the user with a next page link */}
+            {searchMatches.length > 0 &&
+            searchMatches.length < 10 && (
+               <Link
+                  className='pagination-next'
+                  to={nextUrl}
+                  disabled={last}
+                  style={{
+                     'pointer-events': last ? 'none' : 'auto'
+                  }}>
+                  Next Page
+               </Link>
+            )}
+
             <ul className='pagination-list'>
                {Array(pageCount).fill(null).map((_, i) => {
                   const pageNum = i + 1;
@@ -74,33 +120,34 @@ const IndexPage = ({ pageContext }) => {
 
 export default IndexPage;
 
-// Quicky and hacky search
-function searchPosts(posts, search) {
-   // Store title matches for quick lookup
-   let titles = {};
+// const searchPosts =
+//    // Quicky and hacky search
+//    function searchPosts(posts, search) {
+//       // Store title matches for quick lookup
+//       let titles = {};
 
-   // Get all posts that have a matching title
-   const filterPostsByTitle = posts.filter(post => {
-      const hook = post.node.frontmatter;
-      const titleLowerCase = hook.title.toLowerCase();
-      const doesInclude = titleLowerCase.includes(search.toLowerCase());
-      if (doesInclude) {
-         titles[titleLowerCase] = true;
-         return true;
-      } else {
-         return false;
-      }
-   });
+//       // Get all posts that have a matching title
+//       const filterPostsByTitle = posts.filter(post => {
+//          const hook = post.node.frontmatter;
+//          const titleLowerCase = hook.title.toLowerCase();
+//          const doesInclude = titleLowerCase.includes(search.toLowerCase());
+//          if (doesInclude) {
+//             titles[titleLowerCase] = true;
+//             return true;
+//          } else {
+//             return false;
+//          }
+//       });
 
-   // Get all posts that have a matching description and DON'T match by title
-   const filterPostsByDescription = posts.filter(post => {
-      const hook = post.node.frontmatter;
-      const titleLowerCase = hook.title.toLowerCase();
-      const description = post.node.html || '';
-      return !titles[titleLowerCase] && description.toLowerCase().includes(search.toLowerCase());
-   });
+//       // Get all posts that have a matching description and DON'T match by title
+//       const filterPostsByDescription = posts.filter(post => {
+//          const hook = post.node.frontmatter;
+//          const titleLowerCase = hook.title.toLowerCase();
+//          const description = post.node.html || '';
+//          return !titles[titleLowerCase] && description.toLowerCase().includes(search.toLowerCase());
+//       });
 
-   // Add description matches to end of results
-   const filteredPosts = filterPostsByTitle.concat(filterPostsByDescription);
-   return filteredPosts;
-}
+//       // Add description matches to end of results
+//       const filteredPosts = filterPostsByTitle.concat(filterPostsByDescription);
+//       return filteredPosts;
+//    };
