4bfb6cd2a3ea80e4f05dd79403dd410d34a18d81
Gabe Ragland <gabe.ragland@gmail.com>
2019-08-02 13:33:36 -0700

    Added analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
-exports.onClientEntry = () => {};
+import analytics from "./src/utils/analytics.js";
+
+export function onClientEntry() {}
+
+export function onRouteUpdate({ location }) {
+  analytics.page();
+}
