df94bb5c751b5696989e489589286b220392bdb2
Tucker <trblackw@ncsu.edu>
2019-08-07 21:08:38 -0400

    Convert component to use hooks

--- src/components/EmailSignup.js
+++ src/components/EmailSignup.js
@@ -1,92 +1,70 @@
-import React, { Fragment } from "react";
-import fetch from "unfetch";
-import styled from "styled-components";
+import React, { Fragment, useState } from 'react';
+import fetch from 'unfetch';
+import styled from 'styled-components';
 
-class EmailSignup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      subscribed: false,
-      emailInputValue: ""
-    };
-  }
+const EmailSignup = () => {
+   const [subscribed, setSubscribed] = useState(false);
+   const [emailInputValue, setEmailInputValue] = useState('');
 
-  subscribe = async email => {
-    this.setState({ subscribed: true });
+   const subscribe = async email => {
+      setSubscribed(true);
+      try {
+         await fetch('/api/subscribe', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email })
+         });
+      } catch (error) {
+         console.error(error);
+      }
+   };
 
-    const response = await new Promise((resolve, reject) => {
-      fetch("/api/subscribe", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email: email })
-      })
-        .then(r => r.json())
-        .then(data => {
-          resolve(data);
-        });
-    });
-  };
-  render() {
-    const { subscribed } = this.state;
-    return (
-      <div className="card">
-        <div className="card-content">
-          {subscribed ? (
-            <div className="has-text-centered has-text-weight-semibold">
-              You are subscribed&nbsp;&nbsp;🎉
-            </div>
-          ) : (
-            <Fragment>
-              <Title>📩&nbsp;&nbsp;Get new recipes in your inbox</Title>
+   const handleSubmit = e => {
+      e.preventDefault();
+      Boolean(emailInputValue) && subscribe(emailInputValue);
+   };
 
-              <form
-                onSubmit={event => {
-                  event.preventDefault();
-                  const email = this.state.emailInputValue;
-                  if (email) {
-                    this.subscribe(email);
-                  }
-                }}
-              >
-                <div className="field has-addons">
-                  <div className="control is-expanded">
-                    <input
-                      type="email"
-                      className="input"
-                      placeholder="Your Email"
-                      onChange={event => {
-                        this.setState({
-                          emailInputValue: event.target.value
-                        });
-                      }}
-                    />
-                  </div>
-                  <div className="control">
-                    <button
-                      className="button is-primary has-text-weight-semibold"
-                      type="submit"
-                    >
-                      Subscribe
-                    </button>
-                  </div>
-                </div>
-              </form>
-              <Extra>Join 5,043 subscribers. No spam ever.</Extra>
-            </Fragment>
-          )}
-        </div>
+   return (
+      <div className='card'>
+         <div className='card-content'>
+            {subscribed ? (
+               <div className='has-text-centered has-text-weight-semibold'>You are subscribed&nbsp;&nbsp;🎉</div>
+            ) : (
+               <Fragment>
+                  <Title>📩&nbsp;&nbsp;Get new recipes in your inbox</Title>
+
+                  <form onSubmit={handleSubmit}>
+                     <div className='field has-addons'>
+                        <div className='control is-expanded'>
+                           <input
+                              type='email'
+                              className='input'
+                              placeholder='Your Email'
+                              onChange={e => setEmailInputValue(e.target.value)}
+                           />
+                        </div>
+                        <div className='control'>
+                           <button className='button is-primary has-text-weight-semibold' type='submit'>
+                              Subscribe
+                           </button>
+                        </div>
+                     </div>
+                  </form>
+                  <Extra>Join 5,043 subscribers. No spam ever.</Extra>
+               </Fragment>
+            )}
+         </div>
       </div>
-    );
-  }
-}
+   );
+};
 
-const Title = styled("div").attrs({ className: "subtitle is-5" })`
+const Title = styled('div').attrs({ className: 'subtitle is-5' })`
   margin-bottom: 1.2rem;
 `;
 
-const Extra = styled("div")`
+const Extra = styled('div')`
   margin-top: 1rem;
   font-size: 0.8rem;
   opacity: 0.7;
