3cacecc2c2e9b4dc628e5160535dcb4d4df6ae54
Gabe Ragland <gabe.ragland@gmail.com>
2020-07-29 14:43:00 -0700

    update dependencies

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,5 +1,5 @@
 import Analytics from "analytics";
-import googleAnalyticsPlugin from "analytics-plugin-ga";
+import googleAnalyticsPlugin from "@analytics/google-analytics";
 
 const analytics = Analytics({
   debug: process.env.NODE_ENV !== "production",
