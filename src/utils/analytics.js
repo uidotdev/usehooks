83973dd67d5d24452d090bd380576109b0c6835b
Benjamin Adam <benjamin.mark.adam@gmail.com>
2022-02-14 12:32:57 -0700

    Proxy the analytics calls

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,13 +1,9 @@
-import Analytics from "analytics";
-import googleAnalyticsPlugin from "@analytics/google-analytics";
-
-const analytics = Analytics({
-  debug: process.env.NODE_ENV !== "production",
-  plugins: [
-    googleAnalyticsPlugin({
-      trackingId: "UA-195006-22",
-    }),
-  ],
-});
+const analytics = {
+  track(...args) {
+    if (typeof window !== "undefined" && window.plausible) {
+      window.plausible.apply(this, [...args]);
+    }
+  },
+};
 
 export default analytics;
