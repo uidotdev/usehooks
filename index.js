4867d7b3bff7bc3655da487908e7a422e3fecc37
Tyler McGinnis <tylermcginnis33@gmail.com>
2023-09-01 07:34:16 -0600

    Update useThrottle

--- index.js
+++ index.js
@@ -1208,12 +1208,12 @@ export function useSet(values) {
 
 export function useThrottle(value, interval = 500) {
   const [throttledValue, setThrottledValue] = React.useState(value);
-  const lastUpdated = React.useRef();
+  const lastUpdated = React.useRef(null);
 
   React.useEffect(() => {
     const now = Date.now();
 
-    if (now >= lastUpdated.current + interval) {
+    if (lastUpdated.current && now >= lastUpdated.current + interval) {
       lastUpdated.current = now;
       setThrottledValue(value);
     } else {
