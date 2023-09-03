c4ec69af864fd2c5dd0b5f31c36fc5818f690800
Tyler McGinnis <tylermcginnis33@gmail.com>
2023-09-03 13:43:57 -0600

    Update useToggle

--- index.js
+++ index.js
@@ -1230,7 +1230,13 @@ export function useThrottle(value, interval = 500) {
 }
 
 export function useToggle(initialValue) {
-  const [on, setOn] = React.useState(initialValue);
+  const [on, setOn] = React.useState(() => {
+    if (typeof initialValue === "boolean") {
+      return initialValue;
+    }
+
+    return Boolean(initialValue);
+  });
 
   const handleToggle = React.useCallback((value) => {
     if (typeof value === "boolean") {
