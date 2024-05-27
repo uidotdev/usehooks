922b16f9b96c7697bd9e936a5b71b2355b494263
Lynn Fisher <lynnfishe@gmail.com>
2024-05-27 12:01:27 -0700

    style tweawks

--- usehooks.com/src/components/CountdownTimer.tsx
+++ usehooks.com/src/components/CountdownTimer.tsx
@@ -67,15 +67,15 @@ const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
   }
 
   return (
-    <div className="countdown flex gap-2 justify-center">
+    <div className="countdown flex gap-1 justify-center text-md">
       {["days", "hours", "minutes", "seconds"].map((unit, index) => (
         <Fragment key={unit}>
           {index > 0 && <span className="h-[2rem] grid place-content-center">:</span>}
           <div className={`${unit} grid grid-cols-2 gap-x-1 gap-y-1.5`}>
-            <span className="h-[2.3rem] aspect-square grid place-content-center rounded-sm bg-brand-beige bg-opacity-10 font-semibold">
+            <span className="h-[2.2em] aspect-[6/7] grid place-content-center rounded-sm bg-brand-beige bg-opacity-10 font-semibold">
               {formatNumber(timeLeft[unit as keyof TimeLeft]).charAt(0)}
             </span>
-            <span className="h-[2.3rem] aspect-square grid place-content-center rounded-sm bg-brand-beige bg-opacity-10 font-semibold">
+            <span className="h-[2.2em] aspect-[6/7] grid place-content-center rounded-sm bg-brand-beige bg-opacity-10 font-semibold">
               {formatNumber(timeLeft[unit as keyof TimeLeft]).charAt(1)}
             </span>
             <p className="col-span-full text-xs">{unit}</p>
